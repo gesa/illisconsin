21e807e0d3d4edeee8be2a59a2e52799ad386b9f
Aki Rose <hi@akibraun.com>
2014-04-08 15:03:47 -0400

    removes the notice, updates the form to actually work.

--- app/_js/_form.js
+++ app/_js/_form.js
@@ -1,13 +1,5 @@
 (function ($, doc, win) {
   "use strict";
 
-  var
-      $form = $('#google-form');
-
-  function completeSubmission() {
-    win.location.pathname = '/rsvp/thanks/';
-  }
-
-  $form.on('submit', completeSubmission);
 
 })(jQuery, document, window);
